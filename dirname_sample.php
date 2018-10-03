7f4516c7115390a62e5962307276063a66b15a2a
Suguru Oki <oki.suguru@hamee.co.jp>
2018-10-03 10:25:15 +0900

    [追加] サンプルを追加

---
+++ dirname_sample.php
@@ -0,0 +1,3 @@
+<?php
+
+var_dump(dirname(__FILE__));
---
+++ foreach.php
@@ -0,0 +1,9 @@
+<?php
+$array1 = [1,2,3,4,5];
+$array2 = [6,7,8,9,10];
+
+foreach($array1, $array2 as $value1, $value2) {
+    echo $value1;
+    echo $value2;
+    echo "\n";
+}
