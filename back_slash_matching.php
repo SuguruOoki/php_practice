76d6aeca4b1433f6332952575092223e9996a16a
SuguruOoki <oki.work0527@gmail.com>
2021-04-13 14:05:17 +0900

    Add back_slack condition

---
+++ back_slash_matching.php
@@ -0,0 +1,9 @@
+<?php
+
+$test = 'JoliCode\Slack\Api\Model\ConversationsInvitePostResponse200';
+
+if ($test === 'JoliCode\Slack\Api\Model\ConversationsInvitePostResponse200') {
+	echo 'test';
+} else {
+	echo 'test else';
+}
